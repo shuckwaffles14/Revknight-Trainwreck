d8e4c328eae3c079f9d8fbb3e60dbec0a4d03abf
Melinda <mkbsultana@gmail.com>
2020-10-09 22:29:48 +1100

    Unity File :D Figured it out

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public int forceConst = 50;
+
+    private bool canJump;
+    private Rigidbody selfRigidbody;
+
+    void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            canJump = true;
+        }
+    }
+}
