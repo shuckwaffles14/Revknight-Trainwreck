e33dacee1ad89bf1153efb905ab5f19b03cc3dca
Melinda <mkbsultana@gmail.com>
2020-11-10 21:24:45 +1100

    1/2 through Ep. 1 of YT Video
    Fixed Main menu, coded the splash screen with noise, added anime character with some animations. Still need to align and code said animations

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -1,88 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class mainMenu : MonoBehaviour
 {
-    #region Variable Names
-    //all the buttons
-    public Button btnStart;
-    public Button btnOptions;
-    public Button btnExit;
-    public Button btnReturn;
-    public Button btnDesktop;
-    public Button btnNo;
-
-    //all the panels 
-    public GameObject startMenu;
-    public GameObject optionsMenu;
-    public GameObject exitMenu;
-    #endregion
-
-    #region Start Panel
-    // Start is called before the first frame update
-    void Start()
+    public void PlayGame()
     {
-        startMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-        exitMenu.SetActive(false);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void Play()
-    {
-        SceneManager.LoadScene("levelOne");
-        SceneManager.UnloadSceneAsync("mainMenu");
-    }
-    #endregion
-
-    #region Options Panel
-    
-    public void Options()
-    {
-        startMenu.SetActive(false);
-        optionsMenu.SetActive(true);
-        btnStart.enabled = false;
-        btnOptions.enabled = false;
-        btnExit.enabled = false;
-        btnReturn.enabled = true;
-    }
-
-    public void Return()
-    {
-        startMenu.SetActive(true);
-        optionsMenu.SetActive(false);
-        btnStart.enabled = true;
-        btnOptions.enabled = true;
-        btnExit.enabled = true;
-        btnReturn.enabled = false;
-    }
-    #endregion
-
-    #region Exit Panel
-
-    public void Exit()
-    {
-        startMenu.SetActive(false);
-        exitMenu.SetActive(true);
-        btnDesktop.enabled = true;
-        btnNo.enabled = true;
-    }
-
-    public void Desktop()
+    public void QuitGame()
     {
         Application.Quit();
     }
-
-    public void No()
-    {
-        startMenu.SetActive(true);
-        exitMenu.SetActive(false);
-        btnStart.enabled = true;
-        btnOptions.enabled = true;
-        btnExit.enabled = true;
-        btnReturn.enabled = false;
-    }
-    #endregion
 }
